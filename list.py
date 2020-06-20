935d41ebc2112cada02927c2372d5fe53da0f363
14kkrason <45915590+14kkrason@users.noreply.github.com>
2020-06-20 19:07:11 +0200

    Komendy listujce nie zostały jeszcze zrobione

--- list.py
+++ list.py
@@ -11,11 +11,12 @@ class List(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #list command and it's subcommands with dictionaries
+    #grupa komend
     @commands.group()
     async def list(self, ctx):
         ''' '''
 
+     #komendy 
     @list.command()
     async def PC(self, ctx):
         conn = await asyncpg.connect(**dane_log)
